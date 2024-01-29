1ada0c53c29786655562da6df996dcf6cfd0b015
chloecombi <129711958+chloecombi@users.noreply.github.com>
2024-01-29 14:47:06 +0800

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,7 @@ export const color = {
     secondary: "#0E1921",
     secondaryLight: "#1A2B37",
     black: "#0A0A0B",
-    white: "#F8F7FF",
-    whiteShade: "#A8ABB4",
+    white: "#FFEED4",
+    whiteShade: "#FFEED4",
     bg:"#0A141B",
 }
